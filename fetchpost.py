8fee56a6344186c2093a3c4fde5a34306cc34e5b
Darshan Bothra <hitmandarshan10@gmail.com>
2025-05-12 13:32:38 +0530

    Final update to fetchpost.py

--- fetchpost.py
+++ fetchpost.py
@@ -9,32 +9,37 @@ load_dotenv()
 def authorizeUser( CLIENT_ID = '<YOUR-CLIENT-ID>', CLIENT_SECRET  ='<YOUR-CLIENT-SECRET>', USER_AGENT = 'HateCommentIdentifer/0.1 by u/MrAwesome_YT'):
     global reddit
     reddit = praw.Reddit(
-    client_id= CLIENT_ID, # Different for every client! Ask user to enter thier id
-    client_secret=CLIENT_SECRET, # Different for every client! Ask user to enter thier id
+    client_id= "RffIzOVmxRTbolDqA0oYLw", # Different for every client! Ask user to enter thier id
+    client_secret="ZHhU98b66ER0QzGtWqXIIMNPuWtl0g", # Different for every client! Ask user to enter thier id
     user_agent=USER_AGENT
-    )   
+    )
 
-
-def fetchPost(url = "<POST-URL>"):
+def fetchPost(url = "https://www.reddit.com/r/NewToReddit/comments/1h9he47/community_recommendations_megathread/"):
 
     # Input URL
     # url = "https://www.reddit.com/r/ipad/comments/1kj3p1n/ipad_mini_any_good/" # It will remain the only input in the program!
 
     # Extract post ID from URL
-    parts = url.split("/")
-    post_id = parts[6]  # Example: '1kj3p1n'
+    try:
+        parts = url.split("/")
+        post_id = parts[6]  # Example: '1kj3p1n'
+    except Exception as e:
+        return "Invalid POST URL"
 
     # Fetch submission by ID
-    submission = reddit.submission(id=post_id)
-    title = submission.title
-    body = submission.selftext
-    op = submission.author
-    score = submission.score
-    subreddit = submission.subreddit
-    nsfw = submission.over_18
-    created = submission.created_utc
-    tag = predictHS.predict(body)
-
+    try: 
+        submission = reddit.submission(id=post_id)
+        title = submission.title
+        body = submission.selftext
+        op = submission.author
+        score = submission.score
+        subreddit = submission.subreddit
+        nsfw = submission.over_18
+        created = submission.created_utc
+        tag = predictHS.predict(body)
+        code = 200
+    except Exception as e:
+        return int(str(e).split(" ")[1])
 
     # === Store all the comments in the post ===
     '''
@@ -89,6 +94,5 @@ def fetchPost(url = "<POST-URL>"):
     return result
 
 if __name__ == "__main__":
-    post = fetchPost() # All the post details
-    print(post)
-    
+    print(authorizeUser())
+    print(fetchPost())   
